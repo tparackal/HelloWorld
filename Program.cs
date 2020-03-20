84011e4e58d2ed891a050310287e61e0ffd47a89
tparackal <tharunparackal@gmail.com>
2020-03-20 17:36:47 -0500

    HelloWorld V2

--- Program.cs
+++ Program.cs
@@ -6,7 +6,14 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! This is Tharun!");
+            string signal = "";
+            while(signal != " ")
+            {
+                Console.WriteLine("Press Space Bar and Enter to exit:");
+                signal = Console.ReadLine(); // prompts user input to exit the program
+            }
+            Console.WriteLine("Done.");
         }
     }
 }
